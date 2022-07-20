3cbf7379b1804585d69411c31aade95503e52c3d
VictoryZhao <victory.zhao@oocl.com>
2022-07-20 08:34:56 +0800

    changes: fuction

--- src/main/java/pos/machine/ItemInfo.java
+++ src/main/java/pos/machine/ItemInfo.java
@@ -1,5 +1,8 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemInfo {
     private final String barcode;
     private final String name;
@@ -11,60 +14,16 @@ public class ItemInfo {
         this.price = price;
     }
 
-    public String getBarcodesAll(List<String> barcodesList) {
-        ArrayList<String> receiptList = new ArrayList<>();
-        receiptList.add("***<store earning no money>Receipt***\n");
-        for(String barcodes : barcodesList){
-            receiptList.add(getBarcodesLine(barcodes));
-            int totalPrice = totalPrice + getBarodesPrice(barcodes);
-        }
-        receiptList.add("----------------------\n");
-        receiptList.add("Total: " + totalPrice + " (yuan)\n");
-        receiptList.add("**********************");
-        return String.format(String.join("%n",  barcodesList));
+    public String getBarcode() {
+        return barcode;
     }
 
-    public String getBarcodesLine(String barcodes){
-        for(String item : itemInfos) {
-            if (barcodes == item.get(0)){
-                return String  barcodesLine = "Name: " + getBarcodesName(barcodes) + ", Quantity: "
-                        + getBarcodesQuantity(barcodes) + ", Unit Price: " + getBarodesUnit(barcodes)
-                        + " (yuan), Subtotal: " + getBarodesPrice(barcodes) + " (yuan)\n";
-            }
-        }
+    public String getName() {
+        return name;
     }
 
-    public String getBarcodesName(String barcodes) {
-        for(String item : itemInfos) {
-            if (barcodes == item.get(0)){
-                return item.get(1);
-            }
-        }
-        return null;
+    public int getPrice() {
+        return price;
     }
-
-    public int getBarcodesQuantity(String barcodes) {
-        int quantity;
-        for(String iteminfo : itemInfos) {
-            if (barcodes == item.get(0)){
-                quantity++;
-            }
-        }
-        return quantity;
-    }
-
-    public int getBarodesUnit(String barcodes) {
-        for(String item : itemInfos) {
-                if (barcodes == item.get(0)){
-                    return item.get(2);
-                }
-        }
-        return null;
-    }
-
-    public int getBarodesPrice(String barcodes) {
-        return getBarodesUnit(barcodes) * getBarodesQuantity(barcodes);
-    }
-
 
 }
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,5 +1,6 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
@@ -7,4 +8,64 @@ public class PosMachine {
 
         return getBarcodesAll(barcodes);
     }
+
+    public String getBarcodesAll(List<String> barcodesList) {
+        ArrayList<String> receiptList = new ArrayList<>();
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        int totalPrice = 0;
+        receiptList.add("***<store earning no money>Receipt***\n");
+        for(ItemInfo barcodes : itemInfos){
+            receiptList.add(getBarcodesLine(barcodes));
+            totalPrice = totalPrice + getBarodesPrice(barcodes);
+        }
+        receiptList.add("----------------------\n");
+        receiptList.add("Total: " + totalPrice + " (yuan)\n");
+        receiptList.add("**********************");
+        return String.format(String.join("%n",  barcodesList));
+    }
+
+    public String getBarcodesLine(String barcodes){
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        for(ItemInfo item : itemInfos) {
+            if (barcodes == item.getBarcode()){
+                return "Name: " + getBarcodesName(barcodes) + ", Quantity: "
+                        + getBarcodesQuantity(barcodes) + ", Unit Price: " + getBarodesUnit(barcodes)
+                        + " (yuan), Subtotal: " + getBarodesPrice(barcodes) + " (yuan)\n";
+            }
+        }
+    }
+
+    public String getBarcodesName(String barcodes) {
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        for(ItemInfo item : itemInfos) {
+            if (barcodes == item.getBarcode()){
+                return item.getName();
+            }
+        }
+        return null;
+    }
+
+    public int getBarcodesQuantity(List<String>  barcodesList, String barcodes) {
+        int quantity = 0;
+        for(String iteminfo : barcodesList) {
+            if (barcodes == iteminfo){
+                quantity++;
+            }
+        }
+        return quantity;
+    }
+
+    public int getBarodesUnit(String barcodes) {
+        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+        for(ItemInfo item : itemInfos) {
+            if (barcodes == item.getBarcode()){
+                return item.getPrice();
+            }
+        }
+    }
+
+    public int getBarodesPrice(List<String>  barcodesList, String barcodes) {
+        return getBarodesUnit(barcodes) * getBarcodesQuantity(barcodesList, barcodes);
+    }
+
 }
